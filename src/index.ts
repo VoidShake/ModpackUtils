ea821ad8771eba29d6666d816ab88f86ef59229a
Niklas Widmann <nik@somethingcatchy.net>
2024-01-29 17:44:47 +0100

    update cli

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,23 @@
 import { getInput, info, setFailed } from '@actions/core'
-import {
-   Action,
-   CurseforgeService,
-   WebOptions,
-   WebService,
-   defaultPaths
-} from '@voidshake/modpack-cli'
+import { WebService } from '@voidshake/modpack-cli'
 import { getReleaseData } from './release'
 
 async function run() {
-   const actions = getInput('actions', { required: true })
-      .split(',')
-      .map(it => it.trim().toLowerCase())
-
    const release = getReleaseData()
+   const web = new WebService({
+      params: [],
+      webToken: getInput('web_token', { required: true }),
+      apiUrl: getInput('api_url') || undefined,
+      modrinthToken: getInput('modrinth_token') || undefined,
+      curseforgeToken: getInput('curseforge_token') || undefined,
+      ...release,
+   })
+
+   await web.updateWeb()
 
    if (release) {
       info(`found release ${release.version}`)
-   }
-
-   const webOptions = (required: boolean): WebOptions => ({
-      webToken: getInput('web_token', { required }),
-      apiUrl: getInput('api_url') || undefined,
-   })
-
-   if (actions.includes(Action.CURSEFORGE)) {
-      if (!release) throw new Error('curseforge action can only be triggered on release')
-
-      const curseforge = new CurseforgeService({
-         ...webOptions(false),
-         curseforgeToken: getInput('curseforge_token', { required: true }),
-         curseforgeProject: Number.parseInt(getInput('curseforge_project', { required: true })),
-         paths: defaultPaths,
-      })
-
-      await curseforge.createRelease(release)
-   }
-
-   if (actions.includes(Action.WEB)) {
-      const web = new WebService(webOptions(true))
-
-      await web.updateWeb()
-
-      if (release) {
-         await web.createRelease(release)
-      }
+      await web.parseAndCreateRelease()
    }
 }
 
